0b3ef79b1b185d9e5118cd7e7c0ffb6992b2d8b9
Cillian Wing <cwing@zagmail.gonzaga.edu>
2020-01-26 13:15:39 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
 // create your App component here
+import React, { PureComponent } from 'react'
+
+export default class App extends PureComponent {
+
+	componentDidMount() {
+		fetch('http://api.open-notify.org/astros.json')
+	}
+
+	render() {
+
+	}
+
+}
